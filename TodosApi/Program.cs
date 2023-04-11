9466efd8bccb2e23ae35348c5184640e895bdda8
Ryan Babida <ryan.babida@gmail.com>
2023-04-11 09:13:09 -0700

    Add EFCore and SqlServer

--- TodosApi/Program.cs
+++ TodosApi/Program.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 class Program
 {
     public static List<Todo> _Todos = new List<Todo>
@@ -10,6 +13,7 @@ class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services
+            .AddDbContext<AppContext>()
             .AddGraphQLServer()
             .AddQueryType<Query>()
             .AddMutationType<Mutation>();
@@ -25,19 +29,21 @@ class Program
 public class Todo
 {
     public string Name { get; set; }
-    public User User { get; set; }
+    public Status Status { get; set; }
 }
 
-public class User
+public enum Status
 {
-    public string Name { get; set; }
+    Incomplete = 0,
+    InProgress,
+    Done
 }
 
 public class Query
 {
     public List<Todo> GetTodos()
     {
-        var todos = new MockTodoDataStore();
+        var todos = new TodoDataStore();
         return todos.GetTodos();
     }
 }
@@ -46,7 +52,7 @@ public class Mutation
 {
     public Todo AddTodo(string name)
     {
-        var todos = new MockTodoDataStore();
+        var todos = new TodoDataStore();
         return todos.AddTodo(name);
     }
 }
@@ -63,8 +69,54 @@ public class MockTodoDataStore : ITodoDataStore
     }
 }
 
+public class TodoDataStore : ITodoDataStore
+{
+    public List<Todo> GetTodos()
+    {
+        using var appContext = new AppContext();
+        var todoEntities = appContext.Todos.ToList();
+
+        return todoEntities.Select(te =>
+            new Todo
+            {
+                Name = te.Name,
+                Status = (Status)te.Status
+            })
+        .ToList();
+    }
+
+    public Todo AddTodo(string name)
+    {
+        using var appContext = new AppContext();
+        var todoEntity = new TodoEntity() { Name = name };
+        appContext.Todos.Add(todoEntity);
+        appContext.SaveChanges();
+
+        return new Todo { Name = todoEntity.Name };
+    }
+}
+
 public interface ITodoDataStore
 {
     public List<Todo> GetTodos();
     public Todo AddTodo(string name);
+}
+
+public class AppContext : DbContext
+{
+    public DbSet<TodoEntity> Todos => Set<TodoEntity>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,57000;Database=App;User Id=sa;Password=pa33word!;TrustServerCertificate=True;");
+    }
+}
+
+[Table("Todos")]
+public class TodoEntity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Status { get; set; }
 }
