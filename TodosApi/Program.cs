780277ff168874ed513baf97bc989fe8bd944793
Ryan Babida <ryan.babida@gmail.com>
2023-04-14 20:51:43 -0700

    Add TodoService using DI

--- TodosApi/Program.cs
+++ TodosApi/Program.cs
@@ -8,11 +8,14 @@ class Program
         new Todo { Name = "Make breakfast" },
         new Todo { Name = "Study GraphQL" }
     };
+
     static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services
+            .AddScoped<ITodoService>(
+                ts => new TodoService(new TodoDataStore()))
             .AddDbContext<AppContext>()
             .AddGraphQLServer()
             .AddQueryType<Query>()
@@ -22,13 +25,18 @@ class Program
 
         app.MapGraphQL();
 
+        app.MapGet("/todos", (ITodoService todoService) =>
+        {
+            return todoService.GetTodos();
+        });
+
         app.Run();
     }
 }
 
 public class Todo
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public Status Status { get; set; }
 }
 
@@ -44,7 +52,7 @@ public class Query
     public List<Todo> GetTodos()
     {
         var todos = new TodoDataStore();
-        return todos.GetTodos();
+        return todos.GetTodos().Result;
     }
 }
 
@@ -53,44 +61,70 @@ public class Mutation
     public Todo AddTodo(string name)
     {
         var todos = new TodoDataStore();
-        return todos.AddTodo(name);
+        return todos.AddTodo(name).Result;
     }
+}
+
+public class TodoService : ITodoService
+{
+    private ITodoDataStore TodoDataStore { get; set; }
+    public TodoService(ITodoDataStore todoDataStore)
+    {
+        TodoDataStore = todoDataStore;
+    }
+
+    public IResult GetTodos()
+    {
+        var todos = TodoDataStore.GetTodos().Result;
+        return Results.Ok(todos);
+    }
+
+    public IResult AddTodo(string name)
+    {
+        var todo = TodoDataStore.AddTodo(name).Result;
+        return Results.Created("", todo);
+    }
+}
+
+public interface ITodoService
+{
+    public IResult GetTodos();
+    public IResult AddTodo(string name);
 }
 
 public class MockTodoDataStore : ITodoDataStore
 {
-    public List<Todo> GetTodos() => Program._Todos;
+    public Task<List<Todo>> GetTodos()
+    {
+        return Task.FromResult(Program._Todos);
+    }
 
-    public Todo AddTodo(string name)
+    public Task<Todo> AddTodo(string name)
     {
         var todo = new Todo { Name = name };
         Program._Todos.Add(todo);
-        return todo;
+        return Task.FromResult(todo);
     }
 }
 
 public class TodoDataStore : ITodoDataStore
 {
-    public List<Todo> GetTodos()
+    public async Task<List<Todo>> GetTodos()
     {
         using var appContext = new AppContext();
-        var todoEntities = appContext.Todos.ToList();
+        var todoEntities = await appContext.Todos.ToListAsync();
 
-        return todoEntities.Select(te =>
-            new Todo
-            {
-                Name = te.Name,
-                Status = (Status)te.Status
-            })
-        .ToList();
+        return todoEntities
+            .Select(te => new Todo { Name = te.Name, Status = (Status)te.Status })
+            .ToList();
     }
 
-    public Todo AddTodo(string name)
+    public async Task<Todo> AddTodo(string name)
     {
         using var appContext = new AppContext();
         var todoEntity = new TodoEntity() { Name = name };
         appContext.Todos.Add(todoEntity);
-        appContext.SaveChanges();
+        await appContext.SaveChangesAsync();
 
         return new Todo { Name = todoEntity.Name };
     }
@@ -98,8 +132,8 @@ public class TodoDataStore : ITodoDataStore
 
 public interface ITodoDataStore
 {
-    public List<Todo> GetTodos();
-    public Todo AddTodo(string name);
+    public Task<List<Todo>> GetTodos();
+    public Task<Todo> AddTodo(string name);
 }
 
 public class AppContext : DbContext
@@ -108,7 +142,9 @@ public class AppContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=localhost,57000;Database=App;User Id=sa;Password=pa33word!;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,57000;Database=App;User Id=sa;Password=pa33word!;TrustServerCertificate=True;"
+        );
     }
 }
 
@@ -117,6 +153,6 @@ public class TodoEntity
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int Status { get; set; }
 }
