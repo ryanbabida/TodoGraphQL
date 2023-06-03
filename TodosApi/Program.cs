49956e186104fd2f004b74dc1531956a31486d84
Ryan Babida <ryan.babida@gmail.com>
2023-06-02 18:46:30 -0700

    format code

--- TodosApi/Program.cs
+++ TodosApi/Program.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-class Program {
-    public static List<Todo> _Todos = new List<Todo> {
+internal sealed class Program
+{
+    public static List<Todo> _Todos = new()
+    {
         new Todo { Name = "Make breakfast" },
         new Todo { Name = "Study GraphQL" }
     };
 
-    static void Main(string[] args) {
+    private static void Main(string[] args)
+    {
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services
@@ -28,78 +31,85 @@ class Program {
     }
 }
 
-public class Todo {
+public class Todo
+{
     public string Name { get; set; } = "";
     public Status Status { get; set; }
 }
 
-public enum Status {
+public enum Status
+{
     Unknown,
     Incomplete,
     InProgress,
     Done
 }
 
-public class Query {
-    public List<Todo> GetTodos([Service] ITodoDataStore dataStore) {
-        return dataStore.GetTodosAsync().Result;
-    }
+public class Query
+{
+    public List<Todo> GetTodos([Service] ITodoDataStore dataStore) 
+        => dataStore.GetTodosAsync().Result;
 }
 
-public class Mutation {
-    public Todo AddTodo([Service] ITodoDataStore dataStore, string name) {
-        return dataStore.AddTodoAsync(name).Result;
-    }
+public class Mutation
+{
+    public Todo AddTodo([Service] ITodoDataStore dataStore, string name) => dataStore.AddTodoAsync(name).Result;
 }
 
-public class TodoService : ITodoService {
+public class TodoService : ITodoService
+{
     private ITodoDataStore TodoDataStore { get; set; }
-    public TodoService(ITodoDataStore todoDataStore) {
-        TodoDataStore = todoDataStore;
-    }
+    public TodoService(ITodoDataStore todoDataStore) => this.TodoDataStore = todoDataStore;
 
-    public async Task<IResult> GetTodosAsync() {
-        var todos = await TodoDataStore.GetTodosAsync();
+    public async Task<IResult> GetTodosAsync()
+    {
+        var todos = await this.TodoDataStore.GetTodosAsync();
         return Results.Ok(todos);
     }
 
-    public async Task<IResult> AddTodoAsync(string name) {
-        var todo = await TodoDataStore.AddTodoAsync(name);
+    public async Task<IResult> AddTodoAsync(string name)
+    {
+        var todo = await this.TodoDataStore.AddTodoAsync(name);
         return Results.Created("", todo);
     }
 }
 
-public interface ITodoService {
+public interface ITodoService
+{
     public Task<IResult> GetTodosAsync();
     public Task<IResult> AddTodoAsync(string name);
 }
 
-public class MockTodoDataStore : ITodoDataStore {
-    public Task<List<Todo>> GetTodosAsync() {
-        return Task.FromResult(Program._Todos);
-    }
+public class MockTodoDataStore : ITodoDataStore
+{
+    public Task<List<Todo>> GetTodosAsync() => Task.FromResult(Program._Todos);
 
-    public Task<Todo> AddTodoAsync(string name) {
+    public Task<Todo> AddTodoAsync(string name)
+    {
         var todo = new Todo { Name = name };
         Program._Todos.Add(todo);
         return Task.FromResult(todo);
     }
 }
 
-public class TodoDataStore : ITodoDataStore {
-    public async Task<List<Todo>> GetTodosAsync() {
+public class TodoDataStore : ITodoDataStore
+{
+    public async Task<List<Todo>> GetTodosAsync()
+    {
         using var appContext = new AppContext();
         var todoEntities = await appContext.Todos.ToListAsync();
 
         return todoEntities
-            .Select(te => new Todo { 
-                Name = te.Name, 
-                Status = (Status)te.Status 
+            .Select(te => new Todo
+            {
+                Name = te.Name,
+                Status = (Status)te.Status
             })
             .ToList();
     }
 
-    public async Task<Todo> AddTodoAsync(string name) {
+    public async Task<Todo> AddTodoAsync(string name)
+    {
         using var appContext = new AppContext();
         var todoEntity = new TodoEntity() { Name = name };
         appContext.Todos.Add(todoEntity);
@@ -109,23 +119,24 @@ public class TodoDataStore : ITodoDataStore {
     }
 }
 
-public interface ITodoDataStore {
+public interface ITodoDataStore
+{
     public Task<List<Todo>> GetTodosAsync();
     public Task<Todo> AddTodoAsync(string name);
 }
 
-public class AppContext : DbContext {
-    public DbSet<TodoEntity> Todos => Set<TodoEntity>();
+public class AppContext : DbContext
+{
+    public DbSet<TodoEntity> Todos => this.Set<TodoEntity>();
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        optionsBuilder.UseSqlServer(
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(
             "Server=localhost,57000;Database=App;User Id=sa;Password=pa33word!;TrustServerCertificate=True;"
         );
-    }
 }
 
 [Table("Todos")]
-public class TodoEntity {
+public class TodoEntity
+{
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Name { get; set; } = "";
